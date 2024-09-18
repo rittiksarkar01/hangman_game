a9fed4fcbf4601fde935ce62ad25b93632e10b08
Rittik Sarkar <113532392+rittiksarkar01@users.noreply.github.com>
2024-09-18 23:58:30 +0530

    Add files via upload

---
+++ hangman_game.py
@@ -0,0 +1,73 @@
+import random
+from hangman_word import word_list
+from hangman_art import stages, logo
+
+
+
+
+
+def play_hangman():
+    lives = 6
+
+    print(logo)
+
+    chosen_word = random.choice(word_list)
+    #print(chosen_word)
+
+    placeholder = ""
+    word_length = len(chosen_word)
+    for position in range(word_length):
+        placeholder += "_"
+    print("Word to guess: " + placeholder)
+
+    game_over = False
+    correct_letters = []
+
+    while not game_over:
+
+        print(f"****************************{lives}/6 LIVES LEFT****************************")
+        guess = input("Guess a letter: ").lower()
+
+        if guess in correct_letters:
+            print(f"You've already guessed {guess}")
+
+        display = ""
+
+        for letter in chosen_word:
+            if letter == guess:
+                display += letter
+                correct_letters.append(guess)
+            elif letter in correct_letters:
+                display += letter
+            else:
+                display += "_"
+
+        print("Word to guess: " + display)
+
+        if guess not in chosen_word:
+            lives -= 1
+            print(f"You guessed {guess}, that's not in the word. You lose a life.")
+
+            if lives == 0:
+                game_over = True
+
+                print(f"***********************IT WAS {chosen_word}! YOU LOSE**********************")
+
+        if "_" not in display:
+            game_over = True
+            print("****************************YOU WIN****************************")
+
+        print(stages[lives])
+
+  # Check if the user wants to restart
+    restart = input("Would you like to play again? (y/n): ").lower()
+    if restart == 'y':
+      play_hangman()
+
+    # Start the game
+    
+    
+play_hangman()
+
+
+
